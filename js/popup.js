93860a81397523b64ca5f293aa7527eca7fb9c68
Bukowski12 <bukowski12@gmail.com>
2025-12-30 20:58:19 +0100

    refactor: Update Chrome API calls and improve search query URL and data validation.

--- js/popup.js
+++ js/popup.js
@@ -1,20 +1,8 @@
 // Variables
-var extensionVersion = chrome.app.getDetails().version;
-
-// Google Analytics
-//var _gaq = _gaq || [];
-//_gaq.push(['_setAccount', 'UA-18127823-1']);
-//_gaq.push(['_trackPageview']);
-//(function() {
-//    var ga = document.createElement('script');
-//    ga.type = 'text/javascript';
-//    ga.async = true;
-//    ga.src = 'https://ssl.google-analytics.com/ga.js';
-//    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
-//})();
+var extensionVersion = chrome.runtime.getManifest().version;
 
 // Search
-window.onload = function() {
+window.onload = function () {
     // language
     document.getElementById('searchButton').value = chrome.i18n.getMessage("button_search");
 
@@ -22,13 +10,15 @@ window.onload = function() {
     $("input[type=hidden][name=ver]").val(extensionVersion);
 
     // selected text
-    chrome.tabs.query({ active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
-        function(tab) {
-            chrome.tabs.sendMessage(tab[0].id, { method: "getSelection" },
-                function(response) {
-                    if (typeof response !== 'undefined' && response.data != "") {
-                        chrome.tabs.create({ url: 'https://csfd.cz/hledat/?q=' + response.data });
-                    }
-                });
+    chrome.tabs.query({ active: true, currentWindow: true },
+        function (tab) {
+            if (tab && tab[0]) {
+                chrome.tabs.sendMessage(tab[0].id, { method: "getSelection" },
+                    function (response) {
+                        if (typeof response !== 'undefined' && response.data && response.data.trim() !== "") {
+                            chrome.tabs.create({ url: 'https://www.csfd.cz/hledat/?q=' + encodeURIComponent(response.data.trim()) });
+                        }
+                    });
+            }
         });
 };
